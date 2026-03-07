512a814eb7a482b1bc06f04397c5d9d68db07e3f
Suhaani Singh <suhaanis47@gmail.com>
2026-03-07 15:41:15 -0500

    add react router routing structure with protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
-import { Routes, Route } from 'react-router-dom';
-import './App.css'
-import Chatbot from './Chatbot';
-import Checklist from './Checklist';
-import Dashboard from './Dashboard';
-import Map from './Map';
-import Quiz from './Quiz';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import AppLayout from "./components/layout/AppLayout";
+import LandingPage from "./pages/LandingPage";
+import DashboardPage from "./pages/DashboardPage";
+import ChecklistPage from "./pages/ChecklistPage";
+import ChatbotPage from "./pages/ChatbotPage";
+import MapPage from "./pages/MapPage";
+import OnboardingPage from "./pages/OnboardingPage";
 
-function App() {
-
-  return (
-    <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/Chatbot" element={<Chatbot />} />
-      <Route path="/Checklist" element={<Checklist />} />
-      <Route path="/Map" element={<Map />} />
-      <Route path="/Quiz" element={<Quiz />} />
-    </Routes>
-  )
+function ProtectedRoute({ children }) {
+  const { isAuthenticated, isLoading } = useAuth0();
+  if (isLoading) return <div>Loading...</div>;
+  return isAuthenticated ? children : <Navigate to="/" />;
 }
 
-export default App
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/onboarding" element={<ProtectedRoute><OnboardingPage /></ProtectedRoute>} />
+        <Route element={<AppLayout />}>
+          <Route path="/dashboard" element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
+          <Route path="/checklist" element={<ProtectedRoute><ChecklistPage /></ProtectedRoute>} />
+          <Route path="/chatbot" element={<ProtectedRoute><ChatbotPage /></ProtectedRoute>} />
+          <Route path="/map" element={<ProtectedRoute><MapPage /></ProtectedRoute>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
